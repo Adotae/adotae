ca2249b63b9ce639f2a9e2381eb51f7218617502
MrChampz <mrchampz@outlook.com>
2021-01-13 02:59:54 -0300

    Adoption validations

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,17 +1,44 @@
 # frozen_string_literal: true
 
 class Adoption < ApplicationRecord
+  before_validation :set_initial_status, on: :create
+
+  # Validations
+  validates :status, presence: true
+  validate  :initial_status?, on: :create
+  validate  :status_is_defined?, on: :update
+  
+  validates :completed_at, absence: true, on: :create
+
   # Relations
-  belongs_to :giver, class_name: 'User', foreign_key: 'giver_id'
-  belongs_to :adopter, class_name: 'User', foreign_key: 'adopter_id', optional: true
+  belongs_to :giver, class_name: "User", foreign_key: "giver_id"
+  belongs_to :adopter, class_name: "User", foreign_key: "adopter_id", optional: true
   belongs_to :associate, optional: true
   belongs_to :pet
 
   # Scopes
-  scope :incomplete, -> { where(status: 'incomplete') }
-  scope :complete, -> { where(status: 'complete') }
+  scope :incomplete, -> { where(status: "incomplete") }
+  scope :complete, -> { where(status: "complete") }
+
+  STATUS = %w[incomplete complete].freeze
 
   def completed?
-    status == 'completed'
+    status == "complete"
+  end
+
+  private
+
+  def set_initial_status
+    self.status = "incomplete"
+  end
+
+  def initial_status?
+    return if status == "incomplete"
+    errors.add(:status, I18n.t("activerecord.errors.models.adoption.attributes.status.invalid"))
+  end
+
+  def status_is_defined?
+    return unless STATUS.exclude?(status)
+    errors.add(:status, I18n.t("activerecord.errors.models.adoption.attributes.status.invalid"))
   end
 end
