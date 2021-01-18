7aee7142962d5316618e9a41e4b037249691322b
MrChampz <mrchampz@outlook.com>
2021-01-18 19:53:48 -0300

    Disable Sentry transactions & unique index into AdminUser cpf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_002153) do
+ActiveRecord::Schema.define(version: 2021_01_18_224551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_002153) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "cpf"
+    t.index ["cpf"], name: "index_admin_users_on_cpf", unique: true
     t.index ["email"], name: "index_admin_users_on_email", unique: true
   end
 
