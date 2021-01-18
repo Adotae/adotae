55548de95ba46693b552c4b049497d356fdeaf18
= <mrchampz@outlook.com>
2021-01-18 18:03:41 -0300

    Refactoring for rubocop linting

--- app/validators/array_length_validator.rb
+++ app/validators/array_length_validator.rb
@@ -2,14 +2,32 @@
 
 class ArrayLengthValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    validate_options
+    @array_size = value&.size || 0
+    record.errors.add(attribute, :too_short, count: minimum) if too_short
+    record.errors.add(attribute, :too_long, count: maximum) if too_long
+  end
+
+  private
+
+  def validate_options
     return unless options.key?(:minimum) || options.key?(:in)
     return unless options.key?(:maximum) || options.key?(:in)
+  end
 
-    array_size = value.try(:size) || 0
-    minimum = options[:minimum] || options[:in].begin
-    maximum = options[:maximum] || options[:in].end
+  def too_short
+    @array_size < minimum
+  end
 
-    record.errors.add(attribute, :too_short, count: minimum) if array_size < minimum
-    record.errors.add(attribute, :too_long, count: maximum)  if array_size > maximum
+  def too_long
+    @array_size > maximum
+  end
+
+  def minimum
+    options[:minimum] || options[:in].begin
+  end
+
+  def maximum
+    options[:maximum] || options[:in].end
   end
 end
